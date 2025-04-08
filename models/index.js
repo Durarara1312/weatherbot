3e06594b4ad2c67d217d9fe13702e14dfb43f051
Ilya ⚡ <fanny-boy1@yandex.ru>
2025-04-08 08:56:28 +0300

    3

--- models/index.js
+++
@@ -1,7 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-
-module.exports = {
-    Subscription: require('./Subscription'),
-    UserStats: require('./UserStats'),
-    WeatherHistory: require('./WeatherHistory')
-};
